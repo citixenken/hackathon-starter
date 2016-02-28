0c9d5667d0cafdaf9150cd6dad428923d9ae33e0
citizen-ken <citizen-ken@users.noreply.github.com>
2016-02-28 14:24:39 +0300

    search mods

--- controllers/search.js
+++ controllers/search.js
@@ -1,15 +1,19 @@
 /**
  * Module dependencies.
  */
-var router = express.Router();
+var express = require('express');
 var Book = require('../models/Book');
+
+
+// expose
+
+var router = module.exports = express.Router();
 
 // GET /searches
 
 router.get('/', function(req, res, next){
   res.render('search');
 });
-
 
 // POST /searches
 
@@ -28,10 +32,12 @@ router.post('/', function(req, res, next){
 
   var query = {};
 
-  for (var k in req.query){
-    query[k] = RegExp(req.query[k], 'i');
+  for (var k in req.body){
+  	if ('_csrf' === k) continue;
+  	var v = req.body[k];
+    if (v) query[k] = RegExp(v, 'i');
   }
-
+  console.log(query);
   Book.find(query, function(err, docs){
     if (err) return next(err);
     res.render('search', { searches: docs });
